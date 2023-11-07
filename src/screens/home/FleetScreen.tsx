2cda91738506b7d7e638af5cc16c81453fe0f0d9
JUtupe <jutuper@gmail.com>
2023-11-07 19:14:21 +0100

    feat: empty states

--- src/screens/home/FleetScreen.tsx
+++ src/screens/home/FleetScreen.tsx
@@ -7,6 +7,8 @@ import {VehicleResponse} from '../../api/responses';
 import {VehicleItem} from '../../components/molecules/VehicleItem';
 import {HomeStackParamList, RootStackParamList} from '../../navigation/screens';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {EmptyState} from '../../components/molecules/EmptyState';
+import NotAMember from '../../assets/icons/not-a-member.svg';
 
 type Props = NativeStackScreenProps<HomeStackParamList, 'Fleet'>;
 
@@ -24,6 +26,13 @@ export const FleetScreen = ({navigation}: Props) => {
         style={CommonStyles.cutoutStyle}
         contentContainerStyle={{gap: 16}}
         data={vehicles}
+        ListEmptyComponent={() => (
+          <EmptyState
+            icon={<NotAMember width={100} height={100} />}
+            title={'Brak pojazdów'}
+            description={'Uzupełnij swoją flotę aby to zmienić!'}
+          />
+        )}
         keyExtractor={item => item.id}
         renderItem={({item}) => (
           <VehicleItem
